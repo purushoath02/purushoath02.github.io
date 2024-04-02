4f8482a569caaa810ffcd66b89e4b1bb8fe14c75
purushoath2002 <purushoath2002@gmail.com>
2024-04-02 17:34:47 +0000

    fixed about me

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleContact(){
     var contactSection = document.getElementById('contact'); 
     contactSection.classList.toggle('contactActive');
 }
+
+function toggleAbout(){
+    var contactSection = document.getElementById('about'); 
+    contactSection.classList.toggle('aboutActive');
+}
