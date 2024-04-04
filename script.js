a1055f3db8586e860129befe7d3d90787b9cfdbe
purushoath2002 <purushoath2002@gmail.com>
2024-04-04 23:03:00 +0000

    changes

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 function toggleContact(){
+    var nav = document.getElementById('nav');
+    nav.classList.add('active')
+    nav.classList.remove('show')
     var contactSection = document.getElementById('contact'); 
     contactSection.classList.toggle('contactActive');
 }
 
 function toggleAbout(){
+    var nav = document.getElementById('nav');
+    nav.classList.add('active')
+    nav.classList.remove('show')
     var aboutSection = document.getElementById('about'); 
     aboutSection.classList.toggle('aboutActive');
     
 }
 
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the navbar and animated elements
+  
     var navbar = document.getElementById("nav");
     var animated = document.getElementById("animation");
-// console.log(animated)
-    // Add click event listener to the whole document
-    document.addEventListener("click", function(event) {
-        navbar.style.display='block';
-        animated.style.display ='none';
-    });
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
 
-    // Prevent animated element from hiding when clicked
-    // animated.addEventListener("click", function(event) {
-    //     event.stopPropagation(); // Prevent event from bubbling up to the document
-    // });
-});
+    document.addEventListener("click", function handleClick(event) {
+        navbar.classList.add('show')
+        navbar.classList.remove('hidden')
+        animated.remove();
+      document.removeEventListener("click", handleClick)
+     console.log(navbar.classList);
+    });
+    navbar.addEventListener("click", function(evenr){
+        if(!about.classList.contains('aboutActive') && !contact.classList.contains('contactActive'))
+        {
+            navbar.classList.add('show');
+            navbar.classList.remove('active')
+        }
+    })
+
+    
+
+})
