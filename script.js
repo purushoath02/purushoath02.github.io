9286c7bca6b2c7a714cf2c3cc2abbbb91cd55122
purushoath2002 <purushoath2002@gmail.com>
2024-04-04 18:47:55 +0000

    changes

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ function toggleContact(){
 }
 
 function toggleAbout(){
-    var contactSection = document.getElementById('about'); 
-    contactSection.classList.toggle('aboutActive');
+    var aboutSection = document.getElementById('about'); 
+    aboutSection.classList.toggle('aboutActive');
+    
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the navbar and animated elements
+    var navbar = document.getElementById("nav");
+    var animated = document.getElementById("animation");
+// console.log(animated)
+    // Add click event listener to the whole document
+    document.addEventListener("click", function(event) {
+        navbar.style.display='block';
+        animated.style.display ='none';
+    });
+
+    // Prevent animated element from hiding when clicked
+    // animated.addEventListener("click", function(event) {
+    //     event.stopPropagation(); // Prevent event from bubbling up to the document
+    // });
+});
